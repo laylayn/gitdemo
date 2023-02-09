f7d9a5f8ce54c5adedb395a25d224a8548f213b4
layan.h <layanhasanhadi@gmail.com>
2023-02-09 16:16:48 +0400

    Commit fixed if statement

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,13 +4,15 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("layan is dumb");
+        System.out.println("choose a person");
+        System.out.println("quickly");
+
 
         Scanner s = new Scanner(System.in);
 
         String Verdict = s.nextLine();
 
-        if (Verdict == "Layan")
+        if (Verdict.equals("Layan")|| Verdict.equals("Amira"))
             System.out.println("dumb");
         else
             System.out.println("cute");
